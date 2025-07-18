ae31b6b3156aba014f138616c5b9fa44587d1eba
Bigmike334 <Miketalbot04@gmail.com>
2025-07-18 08:46:46 -0700

    Update home.jsx

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -3,6 +3,7 @@ import { Link } from "preact-router/match";
 
 import BoxCard from "./boxCard";
 
+// Import your images
 import fireplaceImg from "/src/images/fireplace.jpg";
 import rockWallImg from "/src/images/rock-wall.png";
 import dirtImg from "/src/images/dirt.jpg";
@@ -13,43 +14,51 @@ export default function Home() {
   const executeScroll = () =>
     myRef.current.scrollIntoView({
       behavior: "smooth",
-      inline: "nearest",
+      block: "start",
     });
+
   return (
     <>
-      <div id="banner-wrapper">
-        <div id="banner" className="box container">
-          <div className="row">
-            <div className="col-7 col-12-medium">
-              <h2>We make your yard look greater.</h2>
-              <p>
-                Our website is under construction, it will be complete soon!
-              </p>
-            </div>
-            <div className="col-5 col-12-medium">
-              <ul>
-                <li>
-                  <Link
-                    href="/contact"
-                    className="button large icon solid fa-arrow-circle-right"
-                  >
-                    Contact Us
-                  </Link>
-                </li>
-                <li>
-                  <a
-                    onClick={executeScroll}
-                    className="button alt large icon solid fa-question-circle"
-                  >
-                    More About Us
-                  </a>
-                </li>
-              </ul>
-            </div>
+      {/* Hero Section */}
+      <div id="banner-wrapper" style={{ position: "relative", height: "400px" }}>
+        <div
+          id="banner"
+          className="box container"
+          style={{
+            position: "absolute",
+            top: "50%",
+            left: "50%",
+            transform: "translate(-50%, -50%)",
+            textAlign: "center",
+            color: "white",
+          }}
+        >
+          <h1 style={{ fontSize: "3em", fontWeight: "bold" }}>
+            Transform Your Outdoors
+          </h1>
+          <p style={{ fontSize: "1.2em", marginBottom: "30px" }}>
+            Your dream landscape is just a click away
+          </p>
+          <div>
+            <Link
+              href="/contact"
+              className="button large icon solid fa-arrow-circle-right"
+              style={{ marginRight: "10px" }}
+            >
+              Contact Us
+            </Link>
+            <a
+              onClick={executeScroll}
+              className="button alt large icon solid fa-question-circle"
+            >
+              More About Us
+            </a>
           </div>
         </div>
       </div>
-      <div id="features-wrapper">
+
+      {/* Features Section */}
+      <div id="features-wrapper" style={{ padding: "50px 0", backgroundColor: "#f8f8f8" }}>
         <div className="container">
           <div className="row">
             <BoxCard
@@ -69,68 +78,69 @@ export default function Home() {
         </div>
       </div>
 
-      <div id="main-wrapper">
+      {/* About Us Section */}
+      <div id="main-wrapper" style={{ padding: "50px 0" }}>
         <div className="container">
           <div className="row gtr-200">
-            <div className="col-4 col-12-medium">
-              <div id="sidebar">
-                <section className="widget thumbnails">
-                  <h3>Interesting stuff</h3>
-                  <div className="grid">
-                    <div className="row gtr-50">
-                      <div className="col-6">
-                        <a href="#" className="image fit">
-                          <img src={fireplaceImg} alt="" />
-                        </a>
-                      </div>
-                      <div className="col-6">
-                        <a href="#" className="image fit">
-                          <img src={rockWallImg} alt="" />
-                        </a>
-                      </div>
-                      <div className="col-6">
-                        <a href="#" className="image fit">
-                          <img src={dirtImg} alt="" />
-                        </a>
-                      </div>
-                      <div className="col-6">
-                        <a href="#" className="image fit">
-                          <img src={paversImg} alt="" />
-                        </a>
-                      </div>
-                    </div>
-                  </div>
-                  <Link href="/gallery" className="button icon fa-file-alt">
-                    See More in the Gallery
-                  </Link>
-                </section>
-              </div>
-            </div>
             <div ref={myRef} className="col-8 col-12-medium imp-medium">
-              <div>
-                <section className="last">
-                  <h2 id="aboutUsSection">So what's this all about?</h2>
-                  <p>
-                    This is the <strong>Greater Edge</strong> and we're more
-                    than a landscaping company.
-                  </p>
-                  <p>
-                    Phasellus quam turpis, feugiat sit amet ornare in, hendrerit
-                    in lectus. Praesent semper bibendum ipsum, et tristique
-                    augue fringilla eu. Vivamus id risus vel dolor auctor
-                    euismod quis eget mi. Etiam eu ante risus. Aliquam erat
-                    volutpat. Aliquam luctus mattis lectus sit amet phasellus
-                    quam turpis.
-                  </p>
-                  <a
-                    href="#"
-                    className="button icon solid fa-arrow-circle-right"
-                  >
-                    Continue Reading
-                  </a>
-                </section>
-              </div>
+              <section className="last">
+                <h2 id="aboutUsSection" style={{ fontSize: "2em", fontWeight: "bold" }}>
+                  So what's this all about?
+                </h2>
+                <p style={{ fontSize: "1.1em" }}>
+                  This is the <strong>Greater Edge</strong> and we're more than a landscaping company.
+                </p>
+                <p style={{ fontSize: "1.1em" }}>
+                  Phasellus quam turpis, feugiat sit amet ornare in, hendrerit in lectus. 
+                  Praesent semper bibendum ipsum, et tristique augue fringilla eu.
+                </p>
+                <a
+                  href="#"
+                  className="button icon solid fa-arrow-circle-right"
+                  style={{ marginTop: "20px" }}
+                >
+                  Continue Reading
+                </a>
+              </section>
             </div>
+          </div>
+        </div>
+      </div>
+
+      {/* Gallery Section */}
+      <div id="gallery-wrapper" style={{ padding: "50px 0", backgroundColor: "#e7e7e7" }}>
+        <div className="container">
+          <h2 style={{ textAlign: "center", marginBottom: "30px" }}>Our Recent Projects</h2>
+          <div className="row">
+            {/* Gallery Grid */}
+            {[fireplaceImg, rockWallImg, dirtImg, paversImg].map((img, idx) => (
+              <div className="col-3" key={idx}>
+                <a href="#" className="image fit">
+                  <img
+                    src={img}
+                    alt={`Project ${idx + 1}`}
+                    style={{
+                      width: "100%",
+                      borderRadius: "10px",
+                      transition: "transform 0.3s ease, box-shadow 0.3s ease",
+                    }}
+                    onMouseOver={(e) => {
+                      e.target.style.transform = "scale(1.05)";
+                      e.target.style.boxShadow = "0 4px 20px rgba(0, 0, 0, 0.1)";
+                    }}
+                    onMouseOut={(e) => {
+                      e.target.style.transform = "scale(1)";
+                      e.target.style.boxShadow = "none";
+                    }}
+                  />
+                </a>
+              </div>
+            ))}
+          </div>
+          <div style={{ textAlign: "center", marginTop: "30px" }}>
+            <Link href="/gallery" className="button icon fa-file-alt">
+              See More in the Gallery
+            </Link>
           </div>
         </div>
       </div>
